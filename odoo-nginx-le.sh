3910189fffa0559d8fb3d511e83f43b4956070d6
Lucas L. Soto <4183046-sotolucas@users.noreply.gitlab.com>
2020-07-24 14:44:24 -0300

    [FIX] regex sed expression

--- odoo-nginx-le.sh
+++ odoo-nginx-le.sh
@@ -9,14 +9,14 @@
 #
 ###################################################################################################
 
-# Let’s begin by updating the package lists and installing software-properties-common and NGINX.
-# Commands separated by && will run in succession.
-echo "Updating and installing software-properties-common and NGINX..."
-sudo apt-get update && sudo apt-get install software-properties-common nginx -y
-
 # Now add the repositories Certbot.
 echo "Adding Certbot repositories..."
 sudo add-apt-repository ppa:certbot/certbot -y
+
+# Let’s begin by updating the package lists and installing software-properties-common and NGINX.
+# Commands separated by && will run in succession.
+echo "Updating and installing software-properties-common and NGINX..."
+sudo apt-get update && sudo apt-get install software-properties-common nginx python-certbot-nginx -y
 
 # Go to NGINX sites-* path and delete all. After that, create a dummy server for ACME challenge.
 echo "Removing default NGINX servers and creating server for ACME challenge..."
@@ -27,10 +27,10 @@ sudo wget -O odoo https://raw.githubusercontent.com/sotolucas/odoo-nginx-le/mast
 # Ask user to input domains.
 read -p "Ingrese el dominio principal: " PRI_DOM
 read -p "Ingrese los dominios secundarios: " SEC_DOM
-ALLDOM=$PRI_DOM' '$SEC_DOM
+ALL_DOM=$PRI_DOM' '$SEC_DOM
 # Replace example domain with domains provided by user.
 echo "Replacing example domain with domains provided by user..."
-sudo sed -i 's/foo-bar.calyx-cloud.com.ar/'$ALL_DOM'/' odoo
+sudo sed -i "s/foo-bar.calyx-cloud.com.ar/$ALL_DOM/g" odoo
 
 # Restart NGINX server.
 echo "Restarting NGINX..."
@@ -49,8 +49,8 @@ sudo wget -O odoo https://raw.githubusercontent.com/sotolucas/odoo-nginx-le/mast
 # Make this in to parts as replacing all foo-bar with ALL_DOM would add it to LE certs path, so...
 # First replace example server name for all domains that we need to listen to.
 # Then, replace example server name for main domain which is also certificate's main domain.
-sudo sed -i 's/server_name foo-bar.calyx-cloud.com.ar/'$ALL_DOM'/' odoo
-sudo sed -i 's/foo-bar.calyx-cloud.com.ar/'$PRI_DOM'/' odoo
+sudo sed -i "s/server_name foo-bar.calyx-cloud.com.ar/$ALL_DOM/g" odoo
+sudo sed -i "s/foo-bar.calyx-cloud.com.ar/$PRI_DOM/g" odoo
 
 # Restart NGINX server.
 echo "Restarting NGINX..."
